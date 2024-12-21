d7ed59727f09f0dad3655825be1c973707cf6355
Apr1l1s <apr1l1s.offical@gmail.com>
2024-12-21 21:32:56 +0300

    [ref] rename Borealis to Alpheratz and correct Dockerfile

---
+++ Aurora.EndPoints.AlpheratzBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using Aurora.EndPoints.BorealisBot.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Telegram.Bot;
+
+// Указываем токен для бота
+const string token = "7877107836:AAHMpLMEl_KfWog0jrx-qgKrw2jQFHkB6L8";
+
+var serviceProvider = new ServiceCollection()
+    .AddLogging()
+    .AddSingleton(new TelegramBotClient(token))
+    .AddSingleton<ITelegramBotService, TelegramBotService>()
+    .AddSingleton<MessageScheduler, RandomMessageScheduler>()
+    .AddSingleton<ISubscribersRepository, LocalSubscribersRepository>()
+    .BuildServiceProvider();
+
+// Инициализируем бота
+var botService = serviceProvider.GetService<ITelegramBotService>();
+if (botService == null)
+{
+    throw new ApplicationException("Неудачная попытка подключения телеграм бота");
+}
+botService.StartBotAsync(token);
+
+// Запускаем сервис планирования сообщений
+var messageScheduler = serviceProvider.GetService<MessageScheduler>();
+if (messageScheduler == null)
+{
+    throw new ApplicationException("Неудачная попытка создания расписания");
+}
+messageScheduler.Start();
+            
+Console.WriteLine("Bot is running...");
+Console.ReadLine();
