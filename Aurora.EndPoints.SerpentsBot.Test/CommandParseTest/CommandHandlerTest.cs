4f2c49ca2ee9a37e318b4fee7bb33370c2938560
Александр Сафронов <a.safronov@teledoc.ru>
2025-03-05 13:43:47 +0300

    [ref] тести

---
+++ Aurora.EndPoints.SerpentsBot.Test/CommandParseTest/CommandHandlerTest.cs
@@ -0,0 +1,11 @@
+﻿using Aurora.Domain.Core.Telegram.Commands;
+
+namespace Aurora.EndPoints.SerpentsBot.Test.CommandParseTest;
+
+public class CommandHandlerTest
+{
+    [Theory]
+    [ClassData(typeof(CommandTextGenerator))]
+    public void NewCommandShouldBeWithCorrectType(CommandTextElement commandElement)
+        => Assert.Equal(new TelegramCommand(commandElement.CommandText).TelegramCommandType, commandElement.TelegramCommandType);
+}
