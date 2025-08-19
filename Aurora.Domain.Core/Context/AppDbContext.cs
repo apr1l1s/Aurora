2ae2da643048424dc946e2a0c68a4fb265cb66ce
safronov.a <a.safronov@teledoc.team>
2025-08-19 10:50:58 +0300

    ref: убрал проект из другого репозитория

--- Aurora.Domain.Core/Context/AppDbContext.cs
+++ Aurora.Domain.Core/Context/AppDbContext.cs
@@ -14,5 +14,25 @@ public class AppDbContext
         {
             options.UseNpgsql("Host=postgres;Database=mydatabase;Username=user;Password=password");
         }
+
+        try
+        {
+            if (Database.CanConnect())
+            {
+                Console.WriteLine("✅ Успешное подключение к базе данных.");
+            }
+            else
+            {
+                Console.WriteLine("⚠️ Подключение не удалось. Пытаемся создать базу...");
+                Database.EnsureCreated();
+                Console.WriteLine("✅ База данных успешно создана.");
+                Database.Migrate();
+                Console.WriteLine("✅ Миграции применены и база создана при необходимости.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"❌ Ошибка: {ex.Message}");
+        }
     }
 }
---
+++ Aurora.EndPoints.SerpensBot/Services/Handlers/BadWordHandler.cs
@@ -0,0 +1,14 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Aurora.EndPoints.SerpensBot.Services.Handlers;
+
+public partial class UpdateHandler
+{
+    private Task<Message> HandleBadWords(Message msg)
+        => bot.SendMessage(msg.Chat.Id, "Не матерись, долбаеб", replyParameters: new ReplyParameters()
+        {
+            ChatId = msg.Chat.Id,
+            MessageId = msg.MessageId,
+        });
+}
